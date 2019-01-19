1d40f0fcbcf8d96d799ae3cf9da0a562a01813dd
heytitle <pat.chormai@gmail.com>
2019-01-19 17:00:14 +0100

    use github cdn

--- app/containers/ArtworkSimilarityPage/datasource.js
+++ app/containers/ArtworkSimilarityPage/datasource.js
@@ -25,17 +25,17 @@ module.exports.datasource = {
   ],
   mapping: {
     'moma-artworks-1000--vgg16':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-vgg16-moma-artworks-1000--2018-12-26-10-01-39.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-vgg16-moma-artworks-1000--2018-12-26-10-01-39.npy.json',
     'moma-artworks-1000--resnet152':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-resnet152-moma-artworks-1000--2018-12-26-09-54-07.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-resnet152-moma-artworks-1000--2018-12-26-09-54-07.npy.json',
     'moma-artworks-1000--densenet':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-densenet-moma-artworks-1000--2018-12-26-09-56-26.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-densenet-moma-artworks-1000--2018-12-26-09-56-26.npy.json',
 
     'moma-artworks--vgg16':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-vgg16-moma-artworks--2018-12-25-14-40-03.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-vgg16-moma-artworks--2018-12-25-14-40-03.npy.json',
     'moma-artworks--resnet152':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-resnet152-moma-artworks--2018-12-25-14-42-36.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-resnet152-moma-artworks--2018-12-25-14-42-36.npy.json',
     'moma-artworks--densenet':
-      'https://s3.amazonaws.com/pchormai-public/artwork-similarity/nn-from-densenet-moma-artworks--2018-12-25-14-48-51.npy.json',
+      'https://raw.githubusercontent.com/heytitle/visually-similar-image-search/master/results/nn-from-densenet-moma-artworks--2018-12-25-14-48-51.npy.json',
   },
 };
